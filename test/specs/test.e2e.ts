c60178494d838d26f4d5abcf792ebdf467feea3a
Juan Torres <juantor16@gmail.com>
2025-08-05 18:01:52 -0300

    Commit inicial

---
+++ test/specs/test.e2e.ts
@@ -0,0 +1,8 @@
+
+describe('Test de smoke basico', () => {
+    it('should login with valid credentials', async () => {
+        console.log(' la app esta abierta')
+        await driver.pause(5000)
+    })
+})
+
