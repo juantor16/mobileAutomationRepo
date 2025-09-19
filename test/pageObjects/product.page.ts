5fc244b019282ed6322e4991115aa65dca381625
Juan Torres <juantor16@gmail.com>
2025-09-19 10:15:46 -0300

    Se agrega codigo de clase 2

--- test/pageObjects/product.page.ts
+++ test/pageObjects/product.page.ts
@@ -1,56 +1,56 @@
-export class ProductPage{
-    private get title(){ return $('#productTV')}
-    private get fotoDelProducto(){ return $('~Displays selected product')}
-    private get precioDelProducto(){ return $('#priceTV')}
-    private get contenedorRating(){ return $('#rattingV')}
-    private get listaDeEstrellas(){ return $$("//android.widget.ImageView[contains(@resource-id, 'start')]")}
-    private get listaDeColoresDisponibles(){ return $('~Displays available colors of selected product')}
-    private get botonDeMenos(){ return $('#minusIV')}
-    private get botonDeMas(){ return $('#plusIV')}
-    private get cantidad(){ return $('#noTV')}
-    private get botonAgregarAlCarrito(){ return $('#cartBt')}
-    
-    public async verificarTituloDelProducto(){
+export class ProductPage {
+    private get title() { return $('#productTV') }
+    private get fotoDelProducto() { return $('~Displays selected product') }
+    private get precioDelProducto() { return $('#priceTV') }
+    private get contenedorRating() { return $('#rattingV') }
+    private get listaDeEstrellas() { return $$("//android.widget.ImageView[contains(@resource-id, 'start')]") }
+    private get listaDeColoresDisponibles() { return $('~Displays available colors of selected product') }
+    private get botonDeMenos() { return $('#minusIV') }
+    private get botonDeMas() { return $('#plusIV') }
+    private get cantidad() { return $('#noTV') }
+    private get botonAgregarAlCarrito() { return $('#cartBt') }
+
+    public async verificarTituloDelProducto() {
         expect(await this.title.isDisplayed()).toBe(true)
     }
 
-    public async verificarFotoDelProducto(){
+    public async verificarFotoDelProducto() {
         expect(await this.fotoDelProducto.isDisplayed()).toBe(true)
     }
 
-    public async verificarPrecioDelProducto(){
+    public async verificarPrecioDelProducto() {
         expect(await this.precioDelProducto.isDisplayed()).toBe(true)
     }
 
-    public async verificarContenedorRating(){
+    public async verificarContenedorRating() {
         expect(await this.contenedorRating.isDisplayed()).toBe(true)
     }
 
-    public async verificarListaDeEstrellas(){
+    public async verificarListaDeEstrellas() {
         expect(await this.listaDeEstrellas.length).toBe(5)
     }
 
-    public async verificarListaDeColoresDisponibles(){
+    public async verificarListaDeColoresDisponibles() {
         expect(await this.listaDeColoresDisponibles.isDisplayed()).toBe(true)
     }
 
-    public async verificarBotonDeMenos(){
+    public async verificarBotonDeMenos() {
         expect(await this.botonDeMenos.isDisplayed()).toBe(true)
     }
 
-    public async verificarBotonDeMas(){
+    public async verificarBotonDeMas() {
         expect(await this.botonDeMas.isDisplayed()).toBe(true)
     }
 
-    public async verificarCantidad(){
+    public async verificarCantidad() {
         expect(await this.cantidad.isDisplayed()).toBe(true)
     }
 
-    public async verificarBotonAgregarAlCarrito(){
+    public async verificarBotonAgregarAlCarrito() {
         expect(await this.botonAgregarAlCarrito.isDisplayed()).toBe(true)
     }
 
-    public async verificarQueTodosLosElementosEstenPresentes(){
+    public async verificarQueTodosLosElementosEstenPresentes() {
         await this.verificarTituloDelProducto()
         await this.verificarFotoDelProducto()
         await this.verificarPrecioDelProducto()
@@ -61,5 +61,39 @@ export class ProductPage{
         await this.verificarBotonDeMas()
         await this.verificarCantidad()
         await this.verificarBotonAgregarAlCarrito()
+    }
+
+    public async aumentarCantidad() {
+        await this.botonDeMas.click()
+    }
+
+    public async disminuirCantidad() {
+        await this.botonDeMenos.click()
+    }
+
+    public async obtenerCantidad(): Promise<string> {
+        return await this.cantidad.getText()
+    }
+
+    public async agregarAlCarrito() {
+        await this.botonAgregarAlCarrito.click()
+    }
+
+    public async establecerCantidad(cantidadDeseada: number) {
+        const cantidadActual = parseInt(await this.obtenerCantidad())
+
+        if (cantidadActual < cantidadDeseada) {
+            for (let i = cantidadActual; i < cantidadDeseada; i++) {
+                await this.aumentarCantidad()
+            }
+        } else if (cantidadActual > cantidadDeseada) {
+            for (let i = cantidadActual; i > cantidadDeseada; i--) {
+                await this.disminuirCantidad()
+            }
+        }
+    }
+
+    public async obtenerNombreDelProducto(): Promise<string> {
+        return await this.title.getText()
     }
 }
